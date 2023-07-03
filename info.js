0ea50e5c4182bdc5b9922ef9f5089dfc3af1cd12
DeveloperAdiiG <114342189+DeveloperAdiiG@users.noreply.github.com>
2023-07-03 11:09:48 +0530

    Update info.js

--- info.js
+++ info.js
@@ -1,9 +1,8 @@
 function sendEmail(){
     Email.send({
         SecureToken : "1dc3b14a-4c36-4c4e-9e04-115ff7afd5d5",
-        
-        To : 'cityinn.bmt@gmail.com',
-        From : 'adii.dg.2021@gmail.com',
+        To : "adii.dg.2021@gmail.com",
+        From : "adii.dg.2021@gmail.com",
         Subject : "New Enquiry from : "+document.getElementById("mail").value,
         Body : "Name : "+document.getElementById("name").value
                 +"<br> Email : "+document.getElementById("mail").value
